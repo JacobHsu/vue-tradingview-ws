cb942e4b9718e0b0abf7d33b8504f46fb8cb0dc4
jacobhsu <jacob.hsu.tw@gmail.com>
2022-03-21 15:47:33 +0800

    feat: api axios qs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <el-main>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <el-main v-if="symbol">
+    {{symbolInfo}}
   </el-main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, reactive } from 'vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import { getSymbols } from '@/api'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // },
+  setup() {
+    const symbolList = reactive({})
+    const symbol = ref('')
+    const symbolInfo = ref({})
+    onMounted(async () => {
+
+      const [list, symbolData] = await getSymbols()
+      symbolList.value = list
+      symbol.value = symbolData
+      symbolInfo.value = list[0]
+    })
+    return {
+      symbol,
+      symbolList,
+      symbolInfo,
+    }
+  },  
 }
 </script>
 
